b51e537ceb33c5d34846ab1877c4362ba99a2663
Matt Neary <neary.matt@gmail.com>
2014-06-10 10:30:21 -0400

    multiple file loading and focus on any given file

--- js/app.js
+++ js/app.js
@@ -14,7 +14,47 @@ var vectorFromVertex = function(v) {
 var vectorsFromVertices = function(vs) {
   return vs.map(vectorFromVertex);
 };
-var renderSTL = function(triangles) {
+var objectNames = [];
+var listenForObjectSelect = function() {
+  [].forEach.call(
+    document.querySelectorAll("#objects a"),
+    function(a) {
+      a.addEventListener("click", function(evt) {
+        var focus = a.innerText;
+        evt.preventDefault();
+	objectNames.forEach(function(name) {
+	  if( name == focus ) {
+	    scene.getObjectByName(name).material = new THREE.MeshNormalMaterial();
+	  } else {
+	    scene.getObjectByName(name).material = new THREE.MeshBasicMaterial({
+	      color: 0xc4c4c4, wireframe: true, wireframe_linewidth: 10
+	    });
+	  }
+	});
+      });
+    });
+};
+var renderNames = function(names) {
+  var objects = document.getElementById("objects");
+  objects.innerHTML = "";
+  var ul = document.createElement("ul");
+  var span = document.createElement("span");
+  span.innerText = "Select an object";
+  objects.appendChild(span);
+  names.map(function(n) {
+    var li = document.createElement("li");
+    var a = document.createElement("a");
+    a.innerText = n;
+    a.href = "#";
+    li.appendChild(a);
+    return li;
+  }).forEach(function(li) {
+    ul.appendChild(li);
+  });
+  objects.appendChild(ul);
+  listenForObjectSelect();
+};
+var renderSTL = function(triangles, name) {
   var geo = new THREE.Geometry();
   triangles.forEach(function(triangle, i) {
     var vs = vectorsFromVertices(triangle.vertices);
@@ -23,22 +63,25 @@ var renderSTL = function(triangles) {
   });
   geo.computeFaceNormals();
   var object = new THREE.Mesh(geo, new THREE.MeshNormalMaterial());
+  object.name = name;
   object.overdraw = true;
   object.position.x = 0;
   object.position.y = 0;
   object.position.z = 0;
   scene.add(object);
+  objectNames.push(name);
+  renderNames(objectNames);
 };
 var handleSTL = function(evt) {
-  var files = evt.target.files;
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    var bytes = new Uint8Array(e.target.result);
-    var triangles = parse(bytes).data;
-    renderSTL(triangles);
-  };
-  scene.children.map(scene.remove.bind(scene));
-  reader.readAsArrayBuffer(files[0]);
+  [].forEach.call(evt.target.files, function(file) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var bytes = new Uint8Array(e.target.result);
+      var triangles = parse(bytes).data;
+      renderSTL(triangles, file.name);
+    };
+    reader.readAsArrayBuffer(file);
+  });
 };
 document.getElementById('files').addEventListener('change', handleSTL, false);
 

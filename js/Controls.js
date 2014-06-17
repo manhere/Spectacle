3f59535693cd4b781690f0afea29d354173c8ebf
Matt Neary <neary.matt@gmail.com>
2014-06-17 10:22:47 -0400

    panning interpreted as shifting the focal point

--- js/Controls.js
+++ js/Controls.js
@@ -1,45 +1,35 @@
 var mode = { rotate: true };
 var Controls = function(render, camera, scene, elm) {
   this.sceneRender = render;
-  this.lookingVector = new THREE.Vector3(1, 0, 0);
-  this.position = new THREE.Vector3(-2000, 0, 100);
+  this.focalPoint = new THREE.Vector3(0,0,0);
+  this.rotationVector = new THREE.Vector3(1, 0, 0);
+  this.lookingVector = function(update) {
+    if( update ) {
+      this.rotationVector = update;
+    }
+    return this.rotationVector.clone();
+  };
+  this._position = new THREE.Vector3(-2000, 0, 100);
+  this.position = function() {
+    return this.focalPoint.clone().add(this._position);
+  };
   this.hypZ = function() {
-    return Math.sqrt(_2(this.position.x) + _2(this.position.y) + _2(this.position.z));
+    return Math.sqrt(_2(this._position.x) + _2(this._position.y) + _2(this._position.z));
   };
   this.camera = camera;
   this.scene = scene;
   this.rotate = function(right, left, up, down) {
-    this.lookingVector.add(new THREE.Vector3((right ? 1 : (left ? -1 : 0)) * 0.05, 0, (up ? 1 : (down ? -1 : 0)) * 0.05));
-  };
-  this.move = function(left, right, up, down) {
-    this.position.add(new THREE.Vector3((left ? 1 : (right ? -1 : 0)) * 10, 0, (up ? 1 : (down ? -1 : 0)) * 10));
+    this.lookingVector().add(new THREE.Vector3((right ? 1 : (left ? -1 : 0)) * 0.05, 0, (up ? 1 : (down ? -1 : 0)) * 0.05));
   };
   this.face = function(key) {
-    this.lookingVector = [
+    this.lookingVector([
       new THREE.Vector3(1, 0, 0),
       new THREE.Vector3(0, 1, 0),
       new THREE.Vector3(-1, 0, 0),
       new THREE.Vector3(0, -1, 0),
-      new THREE.Vector3(0, 0, -1)][key - 1];
-    this.position = this.lookingVector.clone().normalize().multiplyScalar(-this.hypZ());
+      new THREE.Vector3(0, 0, -1)][key - 1]);
+    this._position = this.lookingVector().clone().normalize().multiplyScalar(-this.hypZ());
   };
-  document.addEventListener('keydown', function(evt) {
-    if( [65, 68, 87, 83].indexOf(evt.keyCode) != -1) {
-      evt.preventDefault();
-      this.rotate(
-        evt.keyCode == 65, evt.keyCode == 68,
-        evt.keyCode == 87, evt.keyCode == 83);
-    } else if( [37, 38, 39, 40].indexOf(evt.keyCode) != -1 ) {
-      evt.preventDefault();
-      this.move(
-        evt.keyCode == 39, evt.keyCode == 37,
-        evt.keyCode == 38, evt.keyCode == 40);
-    } else if( [49, 50, 51, 52, 53].indexOf(evt.keyCode) != -1 ) {
-      evt.preventDefault();
-      this.face(evt.keyCode - 48);
-    }
-    render();
-  }.bind(this));
   var start;
   elm.addEventListener('mousedown', function(evt) {
     start = [evt.x, evt.y];
@@ -47,8 +37,9 @@ var Controls = function(render, camera, scene, elm) {
   elm.addEventListener('mouseup', function(evt) {
     start = undefined;
   });
+ 
   this.zoom = function(delta) {
-    this.position.add(this.lookingVector.clone().multiplyScalar(-delta));
+    this._position.add(this.lookingVector().clone().multiplyScalar(-delta));
   };
   elm.addEventListener('mousewheel', function(evt) {
     evt.preventDefault();
@@ -63,21 +54,17 @@ var Controls = function(render, camera, scene, elm) {
     if( mode.rotate ) {
       var dxy = -x/10 * Math.PI/50,
 	  dz = y/10 * Math.PI/50;
-      var xy = Math.atan2(this.position.y, this.position.x) + dxy,
-          z  = Math.atan2(this.position.z, sqrt(_2(this.position.x) + _2(this.position.y))) + dz;
+      var xy = Math.atan2(this._position.y, this._position.x) + dxy,
+          z  = Math.atan2(this._position.z, sqrt(_2(this._position.x) + _2(this._position.y))) + dz;
       var zpos = Math.sin(z) * this.hypZ(),
           r = Math.cos(z) * this.hypZ();
-      this.position = new THREE.Vector3(r * Math.cos(xy), r * Math.sin(xy), zpos);
-      this.lookingVector = this.position.clone().normalize().multiplyScalar(-1);
+      this._position = new THREE.Vector3(r * Math.cos(xy), r * Math.sin(xy), zpos);
+      this.lookingVector(this._position.clone().normalize().multiplyScalar(-1));
     } else if( mode.zoom ) {
       this.zoom(y);
     } else {
       // panning
-      var basis = [
-        this.lookingVector.clone().normalize(),
-	new THREE.Vector3(-this.lookingVector.y, this.lookingVector.x, 0),
-	new THREE.Vector3(0, 0, 1)];
-      this.position.add(inBasis(basis, 0, x/6, y/6));
+      this.focalPoint.add(new THREE.Vector3(x, -y, 0));
     }
   }.bind(this));
   [].map.call(
@@ -106,9 +93,9 @@ var Controls = function(render, camera, scene, elm) {
   this.render();
 };
 Controls.prototype.render = function() {
-  this.camera.position = this.position;
+  this.camera.position = this.position();
   this.camera.up = new THREE.Vector3(0,0,1);
-  this.camera.lookAt(this.position.clone().add(this.lookingVector.clone().multiplyScalar(10)));
+  this.camera.lookAt(this.position().clone().add(this.lookingVector().clone().multiplyScalar(10)));
   this.sceneRender(this.scene, this.camera);
   requestAnimationFrame(this.render.bind(this));
 };
